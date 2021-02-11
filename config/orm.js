20a52aeb743101a75958317e798fa4919feac4e7
Evan Chu <echu34@gmail.com>
2021-02-10 20:16:49 -0500

    added structure and some code to config

---
+++ config/orm.js
@@ -0,0 +1,18 @@
+const connection = require('./connection.js');
+
+const orms = {
+    selectAll(){
+
+    },
+
+    insertOne(){
+
+    },
+
+    updateOne(){
+
+    },
+
+}
+
+module.exports = orm;
