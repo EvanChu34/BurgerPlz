5b2ace15e7717b36464aa228350b5a0567124da9
Evan Chu <echu34@gmail.com>
2021-02-10 20:57:09 -0500

    added orms plus functions

--- models/burger.js
+++ models/burger.js
@@ -2,19 +2,19 @@ const orm = require('../config/orm.js');
 
 const burger = {
     all(){
-
+        orm.all()
     },
     
     selectAll(){
-
+        orm.select()
     },
 
     insertOne(){
-
+        orm.insert()
     },
     
     updateOne(){
-
+        orm.update()
     },
 
 }
