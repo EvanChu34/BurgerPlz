b1b171df70fed1e0a7771948a7eea91ab5b63e97
Evan Chu <echu34@gmail.com>
2021-02-10 20:45:57 -0500

    added the models folder. also added to burger js

---
+++ models/burger.js
@@ -0,0 +1,22 @@
+const orm = require('../config/orm.js');
+
+const burger = {
+    all(){
+
+    },
+    
+    selectAll(){
+
+    },
+
+    insertOne(){
+
+    },
+    
+    updateOne(){
+
+    },
+
+}
+
+module.exports = burger;
