623522986d70471e54c1e132ad1917822fc95f9b
Evan Chu <echu34@gmail.com>
2021-02-12 10:36:46 -0500

    orms work

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,29 @@
 const connection = require('./connection.js');
 
+function printQuestionMarks(num) {
+    var arr = [];
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+    return arr.toString();
+}
+
+function objToSql(obj){
+    var arr = [];
+
+    for (var key in obj){
+        var cvalue = obj[key];
+        if(Object.hasOwnProperty.call(obj, key)){
+            if(typeof value === "string" && value.indexOf(" ") >= 0){
+                value = "'" + value + "'";
+            }
+            return arr.toString();
+        }
+    
+    }
+}
+
+
 const orms = {
     selectAll(){
 
@@ -13,6 +37,10 @@ const orms = {
 
     },
 
+    deleteOne(){
+
+    }
+
 }
 
-module.exports = orm;
+module.exports = orms;
