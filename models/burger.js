d0a57e4a87efe3b1fe23119a39dcf20e30ac73c4
Evan Chu <echu34@gmail.com>
2021-02-12 12:37:07 -0500

    adding for the burger models

--- models/burger.js
+++ models/burger.js
@@ -1,22 +1,25 @@
 const orm = require('../config/orm.js');
 
 const burger = {
-    all(){
-        orm.all()
-    },
     
-    selectAll(){
-        orm.select()
+    selectAll: function(cb){
+        orm.selectAll("burgers",function(res){
+            cb(res);
+        });
     },
 
-    insertOne(){
-        orm.insert()
+    insertOne: function(cols,vals,cb) {
+        orm.insertOne("burgers", cols, vals, function(res){
+            cb(res);
+        })
     },
     
-    updateOne(){
-        orm.update()
+    updateOne: function(objColVals, condition, cb){
+        orm.updateOne("burgers", objColVals, condition, function(res){
+            cb(res);
+        })
     },
 
-}
+};
 
 module.exports = burger;
