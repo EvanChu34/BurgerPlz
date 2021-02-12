497ac9f9ebce73c40cb250a56de9bb724d48690a
Evan Chu <echu34@gmail.com>
2021-02-12 15:44:33 -0500

    added to the controller

---
+++ controllers/burger_controller.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const burger = require("../models/burger.js");
+const router = express.Router();
+
+router.get("/", function (req,res){
+    burger.selectAll(function (data){
+
+    })  
+})
+
+
+router.post("/burger", function (req,res){
+    burger.insertOne(
+    [
+        "burger_name"
+    ],
+    [
+        req.body.burger_name
+    ],
+    function(){
+        res.redirect("/")
+    }
+    )  
+})
+
+router.put("/burgers/:id", function (req,res){
+    var condition = "id = " + req.params.id
+    
+    burger.updateOne({
+        devoured: true
+    }, condition, function(data){
+        res.redirect("/");
+    })  
+})
